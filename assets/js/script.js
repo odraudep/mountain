62179f7d4e000566828cc7e42d1dc5b27aa1246b
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-11-10 16:10:06 -0300

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const hsky = document.querySelector('#hsky')
-const hsun = document.querySelector('#hsun')
 const hman = document.querySelector('#hman')
 const hm3 = document.querySelector('#hm3')
 const hm2 = document.querySelector('#hm2')
@@ -9,8 +8,6 @@ const htxtx = document.querySelector('#htxt')
 window.addEventListener('scroll', function() {
   let value = window.scrollY
 
-  console.log(value)
-
   if (value >= 200) {
     htxt.style.opacity = 0
   } else {
@@ -19,7 +16,5 @@ window.addEventListener('scroll', function() {
 
   hm1.style.bottom = value * 0.15 + 'px'
   hm2.style.bottom = -value * 0.15 + 'px'
-  hsun.style.marginTop = -value * 5 + 'px'
   hsky.style.top = value * 0.2 + 'px'
-
 })
